3c1eea0bd4c4e9fc1326832dee8e230defb2e984
MShiers7 <martyshiers7@gmail.com>
2022-12-08 20:17:08 -0600

    feat: getUserByUsername

--- db/users.js
+++ db/users.js
@@ -3,20 +3,29 @@ const client = require("./client");
 // database functions
 
 // user functions
-async function createUser({ username, password }) {
-  
-}
+async function createUser({ username, password }) {}
 
-async function getUser({ username, password }) {
+async function getUser({ username, password }) {}
 
-}
-
-async function getUserById(userId) {
-
-}
+async function getUserById(userId) {}
 
 async function getUserByUsername(userName) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `,
+      [username]
+    );
 
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
@@ -24,4 +33,4 @@ module.exports = {
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
