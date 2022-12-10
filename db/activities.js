f83805995ce91c4d2218e7b7ea430e5fa4e54831
MShiers7 <martyshiers7@gmail.com>
2022-12-10 11:32:17 -0600

    Hot fix

--- db/activities.js
+++ db/activities.js
@@ -1,67 +1,96 @@
 const id = require("faker/lib/locales/id_ID");
-const client = require("./client")
+const client = require("./client");
 
 // database functions
 async function getAllActivities() {
   try {
-    const {rows: activities} = await client.query(`
+    const { rows: activities } = await client.query(`
     SELECT * FROM activities;
-    `)
-    return activities
-  }catch(error) {
-    throw error
+    `);
+    return activities;
+  } catch (error) {
+    throw error;
   }
-
 }
 
 async function getActivityById(id) {
-  
+  try {
+    const {
+      rows: [activities],
+    } = await client.query(
+      `
+        SELECT id, name, description,
+        FROM activities,
+        WHERE id=$1;
+      `,
+      [id]
+    );
+
+    if (!activities) {
+      return null;
+    }
+
+    return activities;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getActivityByName(name) {
-  console.log("Pulling activity by name")
-  try{
-    const { rows: [activitiesIds] } = await client.query(`
+  console.log("Pulling activity by name");
+  try {
+    const {
+      rows: [activitiesIds],
+    } = await client.query(
+      `
     SELECT *
     FROM activities
     WHERE name=$1;
-    `, [name])
+    `,
+      [name]
+    );
     //return await Promise.all(activitiesIds.map(
     //  activities => getActivityById(activity.id)));
-    console.log("Act here lmao", activitiesIds)
-   return activitiesIds 
-} catch (error) {
-  throw error;
+    console.log("Act here", activitiesIds);
+    return activitiesIds;
+  } catch (error) {
+    throw error;
+  }
 }
-} 
-
 
 // select and return an array of all activities
 async function attachActivitiesToRoutines(routines) {
   try {
-    const { rows: [activities] } = await client.query(`
+    const {
+      rows: [activities],
+    } = await client.query(`
       SELECT *
       FROM activities;
     `);
     return activities;
-  } catch(error) {
-    console.log("An error occured while attaching routines to activities")
-    throw error
+  } catch (error) {
+    console.log("An error occured while attaching routines to activities");
+    throw error;
   }
 }
 
 // return the new activity
 async function createActivity({ name, description }) {
   try {
-    const { rows: [activities] } = await client.query(`
+    const {
+      rows: [activities],
+    } = await client.query(
+      `
       INSERT INTO activities(name, description)
       VALUES($1, $2)
       RETURNING *;
-    `, [name, description]);
+    `,
+      [name, description]
+    );
 
     return activities;
   } catch (error) {
-    console.log("An error occured while creating activities")
+    console.log("An error occured while creating activities");
     throw error;
   }
 }
@@ -69,10 +98,7 @@ async function createActivity({ name, description }) {
 // don't try to update the id
 // do update the name and description
 // return the updated activity
-async function updateActivity({ id, ...fields }) {
-
-}
-
+async function updateActivity({ id, ...fields }) {}
 
 module.exports = {
   getAllActivities,
@@ -81,4 +107,4 @@ module.exports = {
   attachActivitiesToRoutines,
   createActivity,
   updateActivity,
-}
+};
