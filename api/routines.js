313fbfeb7bcbb67c3411b8005aa0195441511f8d
Jaron <jaronchretien@yahoo.com>
2022-12-17 12:32:51 -0600

    feat(/app.js) added and setup routers)

--- api/routines.js
+++ api/routines.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const { TestScheduler } = require('jest');
 const router = express.Router();
-
+const {getAllPublicRoutines} = require('../db')
 // GET /api/routines
-
+router.get('/', async(req, res, next) => {
+    try {
+        const publicRoutines = await getAllPublicRoutines()
+        res.send(publicRoutines)
+    }catch(error) {
+        next(error)
+    }
+})
 // POST /api/routines
 
 // PATCH /api/routines/:routineId
