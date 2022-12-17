fe08fe3c461bd4bfee9357c7f17d6d6b15c9796d
MShiers7 <martyshiers7@gmail.com>
2022-12-17 16:36:02 -0600

    RESET

--- api/users.js
+++ api/users.js
@@ -1,24 +1,81 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env;
 
+const { 
+    getUser,
+    createUser,
+    getUserByUsername,
+    getUserById,
+    getPublicRoutinesByUser
+} = require('../db');
 
 // POST /api/users/login
+router.post('/login', async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        const user = await getUser({ username, password });
+
+        if (user) {
+            const token = jwt.sign({ id: user.id, username }, process.env.JWT_SECRET, {
+                expiresIn: '1w',
+            });
+            res.send({ message: "You're logged in!", token });
+        } else {
+            next({
+                name: 'Incorrect User Credentials',
+                message: 'Username or password is incorrect',
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
 
 // POST /api/users/register
 
+router.post('/register', async (req, res, next) => {
+    try {
+        console.log('The body', req.body)
+        const {username, password} = req.body
+        const existingUser = await getUserByUsername(username)
+        console.log("Exising user", existingUser)
+        if(existingUser) {
+           next({
+                error: res.body,
+                name: "IncorrectCredentialsError",
+                message: `A user by that username already exists.`,
+              });
+        }else if (password.length < 8) {
+            next('Password Too Short!')
+        }
+        const user = await createUser({username: username, password: password})
+        const token = jwt.sign(user, JWT_SECRET, {expiresIn: '1w'})
+        console.log('new user here', user)
+        res.send({message: "Success", token: token, user: user })
+    }catch(error) {
+        next(error)
+    }
+})
+
 // GET /api/users/me
+
 
 router.get('/me', async (req, res, next) => {
     //test
-    
     //const getUsers = await getUserByUsername()
     try{
-        const {users} = req.params
-        console.log("heh", users)
-        const getUser = await getUserByUsername(users)
-        res.send({ users })
+        const {users} = req.params;
+        console.log("heh", users);
+        const gettingUser = await getUserByUsername({users})
+        res.send(gettingUser)
     } catch (error){
         next(error);
+        //descontsruct token & validate token
+    }
+})
 
 // GET /api/users/:username/routines
 
@@ -30,10 +87,7 @@ router.get('/:username/routines', async(req, res, next) => {
         res.send(publicRoutines)
     }catch(error) {
         next(error)
-
     }
 })
-
-// GET /api/users/:username/routines
 
 module.exports = router;
