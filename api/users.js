bfcc820cfec6886d303b57dcbb6a1c284e1bd629
Raebaby <78573429+Raebaby@users.noreply.github.com>
2022-12-17 15:25:25 -0600

    Add GETme

--- api/users.js
+++ api/users.js
@@ -1,54 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = process.env;
 
-const { 
-    getUser,
-    createUser,
-    getUserByUsername,
-    getUserById,
-    getPublicRoutinesByUser
-} = require('../db');
 
 // POST /api/users/login
-router.post('/login', async (req, res, next) => {
-    try {
-        const { username, password } = req.body;
-        const user = await getUser({ username, password });
-
-        if (user) {
-            const token = jwt.sign({ id: user.id, username }, process.env.JWT_SECRET, {
-                expiresIn: '1w',
-            });
-            res.send({ message: "You're logged in!", token });
-        } else {
-            next({
-                name: 'Incorrect User Credentials',
-                message: 'Username or password is incorrect',
-            });
-        }
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
-});
 
 // POST /api/users/register
 
 // GET /api/users/me
 
-// GET /api/users/:username/routines
+router.get('/me', async (req, res, next) => {
 
-router.get('/:username/routines', async(req, res, next) => {
-    try {
-        const {username} = req.params
-        console.log("BRUH", username)
-        const publicRoutines = await getPublicRoutinesByUser(username)
-        res.send(publicRoutines)
-    }catch(error) {
-        next(error)
+    //const getUsers = await getUserByUsername()
+    try{
+        const {users} = req.params
+        console.log("heh", users)
+        const getUser = await getUserByUsername(users)
+        res.send({ users })
+    } catch (error){
+        next(error);
     }
 })
+
+// GET /api/users/:username/routines
 
 module.exports = router;
