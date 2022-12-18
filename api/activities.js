46e82fe130e67e90059247eb73a5259bf767ae86
MShiers7 <martyshiers7@gmail.com>
2022-12-17 19:05:19 -0600

    activityId/routines

--- api/activities.js
+++ api/activities.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const id = require('faker/lib/locales/id_ID');
 const router = express.Router();
-const {getAllActivities} = require('../db')
+const {getAllActivities, getActivityById, getPublicRoutinesByActivity} = require('../db')
 
 
 // GET /api/activities/:activityId/routines
+
+router.get('/:activityId/routines', async(req, res, next) => {
+    try {
+        const {activityId} = req.params
+        console.log("AHHHH", activityId)
+        const selectedActivity = getActivityById(activityId)
+        if (!selectedActivity) {
+            next(error);
+        }
+        const publicRoutinesByActivity = await getPublicRoutinesByActivity({id: activityId})
+    
+        console.log("WTF", publicRoutinesByActivity)
+        res.send(publicRoutinesByActivity)
+    }catch(error) {
+        next(error)
+    }
+})
 
 // GET /api/activities
 
