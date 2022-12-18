7eb5e1fbd3eb67047b0f576a756c6594e52704eb
MShiers7 <martyshiers7@gmail.com>
2022-12-17 18:17:17 -0600

    feat: allActivites/api

--- api/activities.js
+++ api/activities.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const {getAllActivities} = require('../db')
+
 
 // GET /api/activities/:activityId/routines
 
 // GET /api/activities
+
+router.get('/', async(req, res, next) => {
+    try {
+        const allActivities = await getAllActivities()
+        res.send(allActivities)
+    }catch(error) {
+        next(error)
+    }
+})
+
 
 // POST /api/activities
 
