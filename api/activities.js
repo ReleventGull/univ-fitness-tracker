a5e20f5f66c180636578a1330ec4e0934aaa7b4e
MShiers7 <martyshiers7@gmail.com>
2022-12-18 19:17:02 -0600

    feat: started api/activities

--- api/activities.js
+++ api/activities.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const id = require('faker/lib/locales/id_ID');
 const router = express.Router();
-const {getAllActivities, getActivityById, getPublicRoutinesByActivity} = require('../db')
+const {getAllActivities, getActivityById, getPublicRoutinesByActivity, getUser, updateActivity} = require('../db')
 
 
 // GET /api/activities/:activityId/routines
@@ -40,6 +40,64 @@ router.get('/', async(req, res, next) => {
 
 // POST /api/activities
 
+router.post('/', async (req, res, next) => {
+    try{
+        const auth = req.header('Authorization')
+
+        if(!auth) {
+            console.log("There is one that failed")
+            res.status(401)
+     next({
+        name:'AuthoizationError',
+        message:'You are not authorized'
+     })
+        }
+        const token = auth.slice(7)
+        console.log('Token is here', token)
+        const {username} = jwt.verify(token, JWT_SECRET)
+        const gettingUser = await getUserByUsername(username)
+        console.log('The user is here', gettingUser)
+        res.send(gettingUser)
+    } catch(error){
+        next(error);
+        
+    }
+})
+
 // PATCH /api/activities/:activityId
+
+router.patch("/:activityId", getUser, async (req, res, next) => {
+    const { activityId } = req.params;
+    const { name, description } = req.body;
+  
+    const updateFields = {};
+  
+    
+
+  
+    if (name) {
+      updateFields.name = name;
+    }
+  
+    if (description) {
+      updateFields.description = description;
+    }
+  
+    try {
+      const originalActivity = await getActivityById(activityId);
+  
+      if (originalActivity.user.id === req.user.id) {
+        const updatedActivity = await updateActivity(activityId, updateFields);
+        res.send({ allActivities: updatedActivity });
+      } else {
+        next({
+          name: "UnauthorizedUserError",
+          message: "You cannot update an activity that is not yours",
+        });
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  });
 
 module.exports = router;
