b3f2977c3627299ec0f964fd7dcc3c300b0e2568
Jaron <jaronchretien@yahoo.com>
2023-01-06 20:34:28 -0600

    hotfix(removedUselessfiles)

--- api/index.js
+++ api/index.js
@@ -7,30 +7,6 @@ const { JWT_SECRET } = process.env;
 router.get('/health', async (req, res, next) => {
 });
 
-router.use(async (req, res, next) => {
-    const prefix = 'Bearer ';
-    const auth = req.header('Authorization');
-
-    if(!auth) {
-        next ()
-    }else if (auth.startsWith(prefix)) {
-        const token = auth.slice(prefix.length)
-        try {
-            const {id} = jwt.verify(token, JWT_SECRET)
-            if (id) {
-                req.user = await getUserById(id)
-                next ()
-            }
-        }catch({name, message}) {
-            next({name, message })
-        }
-    } else {
-        next ({
-            name: 'AuthoizationHeaderError',
-            message: `Authorization token must start with ${ prefix }`
-        })
-    }
-});
 
 // ROUTER: /api/users
 const usersRouter = require('./users');
